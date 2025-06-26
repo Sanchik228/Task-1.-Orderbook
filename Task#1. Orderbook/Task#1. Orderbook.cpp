605f17b7e7712cb73664136f0a7a4baa52acce11
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-26 10:47:43 +0300

    now class Order is a struct

--- Task#1. Orderbook/Task#1. Orderbook.cpp
+++ Task#1. Orderbook/Task#1. Orderbook.cpp
@@ -1,11 +1,16 @@
 ﻿#include <iostream>
 
-class Order {
+struct Order {
 	int user_id;
 	int amount;
 	int prise;
 	bool side;
+
+	Order(int uID, int amt, int pri, bool sd)
+		: user_id(uID), amount(amt), prise(pri), side(sd) {}
 };
+
+
 
 int main() {
 
